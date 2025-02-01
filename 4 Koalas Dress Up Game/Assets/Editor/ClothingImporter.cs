acea5ae1d849a7239bdfa470fe109e7502ededd1
EternalNyaki <124942024+EternalNyaki@users.noreply.github.com>
2025-02-01 15:01:12 -0500

    Created clothing items system
    - Created base clothing item class
    - Created clothing item importer
    - Created clothing item datasheet

---
+++ 4 Koalas Dress Up Game/Assets/Editor/ClothingImporter.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ClothingImporter.asset", menuName = "Dress-Up/Clothing Importer")]
+public class ClothingImporter : ScriptableObject
+{
+    [Tooltip("Path from Assets to Excel File")]
+    public string excelFilePath = "Editor/ClothingData.xlsx";
+
+    public void Import()
+    {
+        float taskStart = Time.realtimeSinceStartup;
+
+        var excel = new ExcelImporter(excelFilePath);
+
+        var items = DataHelper.GetAllAssetsOfType<ClothingItem>();
+
+        ImportItems("ClothingItems", excel, items);
+
+        Debug.Log($"Clothing Items import succeeded in {(Time.realtimeSinceStartup - taskStart) * 1000}ms");
+    }
+
+    private void ImportItems(string category, ExcelImporter excel, Dictionary<string, ClothingItem> items)
+    {
+        if (!excel.TryGetTable(category, out var table))
+        {
+            Debug.LogError($"Could not find {category} table");
+            return;
+        }
+
+        for (int row = 1; row <= table.RowCount; row++)
+        {
+            string name = table.GetValue<string>(row, "Name");
+            if (string.IsNullOrWhiteSpace(name)) { continue; }
+
+            var item = DataHelper.GetOrCreateAsset(name, items, category);
+
+            if (table.TryGetEnum<ClothingType>(row, "Type", out var type))
+            {
+                item.type = type;
+            }
+        }
+    }
+}
+
+
+//Editor class for the clothing importer
+//Create an Import button in the inspector
+[CustomEditor(typeof(ClothingImporter))]
+public class ClothingImporterEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        var editorObject = (ClothingImporter)target;
+
+        if (EditorGUILayout.LinkButton("Import"))
+        {
+            editorObject.Import();
+        }
+    }
+}
---
+++ 4 Koalas Dress Up Game/Assets/Scripts/ClothingItem.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public enum ClothingType
+{
+    Hat,
+    Shirt,
+    Pants,
+    Shoes
+}
+
+[CreateAssetMenu(fileName = "ClothingItem.asset", menuName = "Dress-Up/Clothing Item")]
+public class ClothingItem : ScriptableObject
+{
+    public ClothingType type;
+    public Sprite image;
+}
