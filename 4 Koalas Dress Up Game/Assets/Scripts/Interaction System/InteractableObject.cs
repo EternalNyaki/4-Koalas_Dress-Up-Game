0c56721d90e5c3179def66782fb65c15ff46a985
EternalNyaki <124942024+EternalNyaki@users.noreply.github.com>
2025-02-21 21:45:34 -0500

    Commented and made minor adjustments
    - Commented InteractableObject and Trigger classes
    - Expanded interaction triggerbox to cover the interactable object in all directions

--- 4 Koalas Dress Up Game/Assets/Scripts/Interaction System/InteractableObject.cs
+++ 4 Koalas Dress Up Game/Assets/Scripts/Interaction System/InteractableObject.cs
@@ -1,25 +1,21 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
+//Interactable Object component
+//Component that lets an object output a message to the console which interacted with by the player
 public class InteractableObject : MonoBehaviour
 {
+    //The string that will be output to the console when the interaction is triggered
     public string interactionResponse;
 
-    private PlayerController _interactor = null;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    //Reference to a player that could interact with this object
+    private PlayerController _player = null;
 
     // Update is called once per frame
     void Update()
     {
-        if (_interactor != null && Input.GetKeyDown(KeyCode.Z))
+        if (_player != null && Input.GetKeyDown(KeyCode.Z))
         {
             Interact();
         }
@@ -27,18 +23,23 @@ public class InteractableObject : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        _interactor = collision.gameObject.GetComponent<PlayerController>();
+        //If the triggering collider is a player, get a reference to it
+        _player = collision.gameObject.GetComponent<PlayerController>();
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (_interactor.gameObject == collision.gameObject)
+        //Clear the player reference
+        if (_player.gameObject == collision.gameObject)
         {
-            _interactor = null;
+            _player = null;
         }
     }
 
-    public void Interact()
+    //Interact Method
+    //Writes a message to the console when a player interacts with this object
+    //Can be overriden for alternative functionality
+    public virtual void Interact()
     {
         Debug.Log(gameObject.name + ": \"" + interactionResponse + "\"");
     }
--- 4 Koalas Dress Up Game/Assets/Scripts/Trigger.cs
+++ 4 Koalas Dress Up Game/Assets/Scripts/Trigger.cs
@@ -2,16 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Trigger class
+//Represents a boolean trigger that automatically resets itself whenever the value is checked
+//You can pretty much just treat this as a boolean in most circumstances, as it can be implicitly converted
+//both to and from a boolean value
 public class Trigger
 {
+    //The boolean value of the trigger
     private bool _value;
 
     public static implicit operator bool(Trigger t)
     {
+        //Reset the trigger whenever the value is checked
         bool output = t._value;
         t._value = false;
         return output;
     }
+
     public static implicit operator Trigger(bool b)
     {
         Trigger t = new Trigger();
